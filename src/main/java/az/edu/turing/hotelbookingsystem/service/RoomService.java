b27f21acaf39e6c255618f64d6f82299ce6aac13
NihadVeliyev <nihadv795@gmail.com>
2025-08-07 03:36:39 +0400

    add RoomService class with Spring annotations for dependency injection

--- src/main/java/az/edu/turing/hotelbookingsystem/service/RoomService.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/service/RoomService.java
@@ -1,4 +1,10 @@
 package az.edu.turing.hotelbookingsystem.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class RoomService {
+
 }
