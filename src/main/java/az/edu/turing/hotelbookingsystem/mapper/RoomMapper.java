06519d72e2b351062f6bd719dbb70a2439982d15
NihadVeliyev <nihadv795@gmail.com>
2025-08-07 04:08:09 +0400

    add RoomMapper interface with MapStruct mappings for Room entity conversion

--- src/main/java/az/edu/turing/hotelbookingsystem/mapper/RoomMapper.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/mapper/RoomMapper.java
@@ -1,4 +1,20 @@
 package az.edu.turing.hotelbookingsystem.mapper;
 
-public class RoomMapper {
+import az.edu.turing.hotelbookingsystem.dto.Room.RoomRequest;
+import az.edu.turing.hotelbookingsystem.dto.Room.RoomResponse;
+import az.edu.turing.hotelbookingsystem.entity.Room;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RoomMapper {
+    @Mapping(target = "hotel", source = "hotelId", qualifiedByName = "idToHotel")
+    @Mapping(target = "status", constant = "AVAILABLE")
+    @Mapping(target = "booking", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    Room toEntity(RoomRequest request);
+    RoomResponse toResponse(Room room);
+
+
 }
