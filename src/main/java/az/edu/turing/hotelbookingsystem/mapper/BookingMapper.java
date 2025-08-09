d572e58f9a28cd64a74fcea574d4ee4dc711a3c1
NihadVeliyev <nihadv795@gmail.com>
2025-08-09 18:20:27 +0400

    add BookingMapper interface with MapStruct mappings for Booking entity conversion

--- src/main/java/az/edu/turing/hotelbookingsystem/mapper/BookingMapper.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/mapper/BookingMapper.java
@@ -1,4 +1,15 @@
 package az.edu.turing.hotelbookingsystem.mapper;
 
-public class BookingMapper {
+import az.edu.turing.hotelbookingsystem.dto.Booking.BookingRequest;
+import az.edu.turing.hotelbookingsystem.dto.Booking.BookingResponse;
+import az.edu.turing.hotelbookingsystem.entity.Booking;
+import lombok.AllArgsConstructor;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface BookingMapper {
+
+    Booking toEntity(BookingRequest bookingRequest);
+    BookingResponse toResponse(Booking booking);
 }
+
