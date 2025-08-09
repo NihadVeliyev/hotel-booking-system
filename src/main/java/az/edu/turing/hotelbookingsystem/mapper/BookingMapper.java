63df73cdf367aa4a831bb40467e91cc0a2e40564
NihadVeliyev <nihadv795@gmail.com>
2025-08-09 18:42:52 +0400

    update BookingMapper to ignore specific fields during BookingRequest to Booking conversion

--- src/main/java/az/edu/turing/hotelbookingsystem/mapper/BookingMapper.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/mapper/BookingMapper.java
@@ -3,12 +3,15 @@ package az.edu.turing.hotelbookingsystem.mapper;
 import az.edu.turing.hotelbookingsystem.dto.Booking.BookingRequest;
 import az.edu.turing.hotelbookingsystem.dto.Booking.BookingResponse;
 import az.edu.turing.hotelbookingsystem.entity.Booking;
-import lombok.AllArgsConstructor;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BookingMapper {
-
+    @Mapping(target = "id" , ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "room", ignore = true)
+    @Mapping(target = "bookingStatus", ignore = true)
     Booking toEntity(BookingRequest bookingRequest);
     BookingResponse toResponse(Booking booking);
 }
