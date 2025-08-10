90299c43b57371a5aa506bd94169e20703a04c88
NihadVeliyev <nihadv795@gmail.com>
2025-08-11 01:41:29 +0400

    add method to RoomDAO for fetching rooms by hotel ID

--- src/main/java/az/edu/turing/hotelbookingsystem/dao/RoomDAO.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/dao/RoomDAO.java
@@ -3,5 +3,8 @@ package az.edu.turing.hotelbookingsystem.dao;
 import az.edu.turing.hotelbookingsystem.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomDAO extends JpaRepository<Room,Long> {
+    List<Room> findAllByHotelId(Long hotelId);
 }
--- src/main/java/az/edu/turing/hotelbookingsystem/service/RoomService.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/service/RoomService.java
@@ -3,6 +3,7 @@ package az.edu.turing.hotelbookingsystem.service;
 import az.edu.turing.hotelbookingsystem.dao.HotelDAO;
 import az.edu.turing.hotelbookingsystem.dao.RoomDAO;
 import az.edu.turing.hotelbookingsystem.dto.Room.RoomResponse;
+import az.edu.turing.hotelbookingsystem.exceptions.NotFoundException;
 import az.edu.turing.hotelbookingsystem.mapper.RoomMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister;
@@ -16,6 +17,7 @@ public class RoomService {
     private final RoomDAO roomDAO;
     private final RoomMapper roomMapper;
     private final HotelDAO hotelDAO;
+
 
 
 
