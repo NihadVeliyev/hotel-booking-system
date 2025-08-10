b90dffdd7061f5b17efbed2d6b37778e54bf4423
NihadVeliyev <nihadv795@gmail.com>
2025-08-11 01:26:02 +0400

    add dependencies and fields to RoomService for Room and Hotel data handling

--- src/main/java/az/edu/turing/hotelbookingsystem/service/RoomService.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/service/RoomService.java
@@ -1,10 +1,23 @@
 package az.edu.turing.hotelbookingsystem.service;
 
+import az.edu.turing.hotelbookingsystem.dao.HotelDAO;
+import az.edu.turing.hotelbookingsystem.dao.RoomDAO;
+import az.edu.turing.hotelbookingsystem.dto.Room.RoomResponse;
+import az.edu.turing.hotelbookingsystem.mapper.RoomMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class RoomService {
+    private final RoomDAO roomDAO;
+    private final RoomMapper roomMapper;
+    private final HotelDAO hotelDAO;
+
+
+
 
 }
