70c5e59acf8b8863226c39b511c422a73a60f551
NihadVeliyev <nihadv795@gmail.com>
2025-08-13 02:07:05 +0400

    add BookingService methods to retrieve all bookings by room ID and all bookings

--- src/main/java/az/edu/turing/hotelbookingsystem/dao/BookingDAO.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/dao/BookingDAO.java
@@ -1,9 +1,10 @@
 package az.edu.turing.hotelbookingsystem.dao;
-
 import az.edu.turing.hotelbookingsystem.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface BookingDAO extends JpaRepository <Booking,Long> {
+    List<Booking> findAllByRoomId(Long roomId);
 }
--- src/main/java/az/edu/turing/hotelbookingsystem/service/BookingService.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/service/BookingService.java
@@ -1,4 +1,38 @@
 package az.edu.turing.hotelbookingsystem.service;
+import az.edu.turing.hotelbookingsystem.dao.BookingDAO;
+import az.edu.turing.hotelbookingsystem.dao.RoomDAO;
+import az.edu.turing.hotelbookingsystem.dto.Booking.BookingResponse;
+import az.edu.turing.hotelbookingsystem.entity.Booking;
+import az.edu.turing.hotelbookingsystem.entity.Room;
+import az.edu.turing.hotelbookingsystem.exceptions.RoomNotFoundException;
+import az.edu.turing.hotelbookingsystem.mapper.BookingMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class BookingService {
+    private final BookingDAO bookingDAO;
+    private  final BookingMapper bookingMapper;
+    private final RoomDAO roomDAO;
+    @Transactional(readOnly = true)
+    public List<BookingResponse> getAllBookings(Long id){
+        Room room=roomDAO.findById(id)
+                .orElseThrow(()->new RoomNotFoundException("Room not found with id: "+id));
+        List<Booking> bookingList=bookingDAO.findAllByRoomId(id);
+        return bookingList.stream().map(n->bookingMapper.toResponse(n)).toList();
+    }
+
+    public List<BookingResponse> getAllBookings() {
+        return bookingDAO.findAll().stream()
+                .map(bookingMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+
+
 }
