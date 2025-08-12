7c517c9087824fa15e0f1ed7ab73dbca18d09735
NihadVeliyev <nihadv795@gmail.com>
2025-08-13 02:21:17 +0400

    rename deleteBookingByRoomId method to deleteByRoomId in BookingDAO

--- src/main/java/az/edu/turing/hotelbookingsystem/dao/BookingDAO.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/dao/BookingDAO.java
@@ -7,5 +7,5 @@ import java.util.List;
 @Repository
 public interface BookingDAO extends JpaRepository <Booking,Long> {
     List<Booking> findAllByRoomId(Long roomId);
-    void deleteBookingByRoomId(Long roomId);
+    void deleteByRoomId(Long roomId);
 }
