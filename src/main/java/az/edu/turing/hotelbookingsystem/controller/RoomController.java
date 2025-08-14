10c74133e56cfe5b51332c1fa3074855dfa95994
NihadVeliyev <nihadv795@gmail.com>
2025-08-15 02:03:52 +0400

    update RoomController to add room and delete room by ID endpoints

--- src/main/java/az/edu/turing/hotelbookingsystem/controller/RoomController.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/controller/RoomController.java
@@ -1,4 +1,29 @@
 package az.edu.turing.hotelbookingsystem.controller;
 
+import az.edu.turing.hotelbookingsystem.dto.Room.RoomRequest;
+import az.edu.turing.hotelbookingsystem.dto.Room.RoomResponse;
+import az.edu.turing.hotelbookingsystem.service.RoomService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rooms")
+@RequiredArgsConstructor
 public class RoomController {
+    private final RoomService roomService;
+    @PostMapping
+    public ResponseEntity<RoomResponse> addRoom(@Valid @RequestBody RoomRequest request){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(roomService.addRoom(request));
+    }
+    @DeleteMapping
+    public  ResponseEntity<Void> deleteRoom(@PathVariable Long id){
+        roomService.deleteRoomById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 }
