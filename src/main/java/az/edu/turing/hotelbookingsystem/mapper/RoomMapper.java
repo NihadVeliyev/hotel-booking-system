01ffdb7a2b8a0b62ec7b2252d9a9cdc973376648
NihadVeliyev <nihadv795@gmail.com>
2025-08-15 01:17:05 +0400

    update RoomMapper to ignore hotel and id fields in toEntity method

--- src/main/java/az/edu/turing/hotelbookingsystem/mapper/RoomMapper.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/mapper/RoomMapper.java
@@ -1,11 +1,13 @@
 package az.edu.turing.hotelbookingsystem.mapper;
-
+import az.edu.turing.hotelbookingsystem.dao.HotelDAO;
 import az.edu.turing.hotelbookingsystem.dto.Room.RoomRequest;
 import az.edu.turing.hotelbookingsystem.dto.Room.RoomResponse;
+import az.edu.turing.hotelbookingsystem.entity.Hotel;
 import az.edu.turing.hotelbookingsystem.entity.Room;
+import az.edu.turing.hotelbookingsystem.exceptions.NotFoundException;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
+import org.mapstruct.Named;
 @Mapper(componentModel = "spring")
 public interface RoomMapper {
     @Mapping(target = "status", constant = "AVAILABLE")
@@ -14,8 +16,7 @@ public interface RoomMapper {
     @Mapping(target = "hotel", ignore = true)
     @Mapping(target = "id", ignore = true)
     Room toEntity(RoomRequest request);
-    @Mapping(target = "hotel", source = "hotelId")
+    @Mapping(target = "hotel", ignore = true)
     RoomResponse toResponse(Room room);
-
-
 }
+
