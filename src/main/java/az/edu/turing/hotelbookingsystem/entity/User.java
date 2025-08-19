6174ec31525fad5bf7eba674433a59d9e0111d8f
NihadVeliyev <nihadv795@gmail.com>
2025-08-19 20:24:22 +0400

    add User entity to represent users in the application

---
+++ src/main/java/az/edu/turing/hotelbookingsystem/entity/User.java
@@ -0,0 +1,21 @@
+package az.edu.turing.hotelbookingsystem.entity;
+
+import az.edu.turing.hotelbookingsystem.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true,nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
