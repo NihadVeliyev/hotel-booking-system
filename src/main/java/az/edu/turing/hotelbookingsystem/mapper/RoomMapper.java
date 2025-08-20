7eb8f1923c2287d1ea003ad553966271eef89029
NihadVeliyev <nihadv795@gmail.com>
2025-08-20 23:54:57 +0400

    remove unused imports in RoomMapper.java

--- src/main/java/az/edu/turing/hotelbookingsystem/mapper/RoomMapper.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/mapper/RoomMapper.java
@@ -1,13 +1,9 @@
 package az.edu.turing.hotelbookingsystem.mapper;
-import az.edu.turing.hotelbookingsystem.dao.HotelDAO;
 import az.edu.turing.hotelbookingsystem.dto.Room.RoomRequest;
 import az.edu.turing.hotelbookingsystem.dto.Room.RoomResponse;
-import az.edu.turing.hotelbookingsystem.entity.Hotel;
 import az.edu.turing.hotelbookingsystem.entity.Room;
-import az.edu.turing.hotelbookingsystem.exceptions.NotFoundException;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 @Mapper(componentModel = "spring")
 public interface RoomMapper {
     @Mapping(target = "status", constant = "AVAILABLE")
