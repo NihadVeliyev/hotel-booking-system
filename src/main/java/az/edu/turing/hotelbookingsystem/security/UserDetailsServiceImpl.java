11015d58fbd8cea4e7e7360b3c1dff4c2c0f6f40
NihadVeliyev <nihadv795@gmail.com>
2025-08-21 13:50:57 +0400

    rename UserServiceImpl to UserDetailsServiceImpl for clarity

--- src/main/java/az/edu/turing/hotelbookingsystem/security/UserDetailsServiceImpl.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/security/UserDetailsServiceImpl.java
@@ -2,5 +2,5 @@ package az.edu.turing.hotelbookingsystem.security;
 
 import az.edu.turing.hotelbookingsystem.service.UserService;
 
-public class UserServiceImpl extends UserService {
+public class UserDetailsServiceImpl extends UserService {
 }
