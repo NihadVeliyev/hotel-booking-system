7cef1b3f5621e5ad7262fcbf6d0c54459843e193
NihadVeliyev <nihadv795@gmail.com>
2025-08-25 19:08:49 +0400

    add fields to UserResponse for id, username, and role

--- src/main/java/az/edu/turing/hotelbookingsystem/dto/User/UserResponse.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/dto/User/UserResponse.java
@@ -1,7 +1,11 @@
 package az.edu.turing.hotelbookingsystem.dto.User;
 
+import az.edu.turing.hotelbookingsystem.enums.Role;
 import lombok.Data;
 
 @Data
 public class UserResponse {
+    private Long id;
+    private String username;
+    private Role role;
 }
