b3c3eb621cce34b786868832c485e212b4f2fa80
NihadVeliyev <nihadv795@gmail.com>
2025-08-25 19:07:02 +0400

    add validation annotations to UserRequest for username and password

--- src/main/java/az/edu/turing/hotelbookingsystem/dto/User/UserRequest.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/dto/User/UserRequest.java
@@ -1,7 +1,12 @@
 package az.edu.turing.hotelbookingsystem.dto.User;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserRequest {
+    @NotBlank(message = "Username cannot be empty")
+    private String username;
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
 }
