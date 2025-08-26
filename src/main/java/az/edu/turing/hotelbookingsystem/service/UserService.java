77820f91f8ab5e843e4773917fd3a39b3ced5c0d
NihadVeliyev <nihadv795@gmail.com>
2025-08-26 17:28:25 +0400

    add createUser method to UserService for user creation with logging and transaction management

--- src/main/java/az/edu/turing/hotelbookingsystem/service/UserService.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/service/UserService.java
@@ -1,12 +1,31 @@
 package az.edu.turing.hotelbookingsystem.service;
 
+import az.edu.turing.hotelbookingsystem.dao.UserDAO;
+import az.edu.turing.hotelbookingsystem.dto.User.UserRequest;
+import az.edu.turing.hotelbookingsystem.dto.User.UserResponse;
+import az.edu.turing.hotelbookingsystem.entity.User;
+import az.edu.turing.hotelbookingsystem.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UserService {
+    private final UserDAO userDAO;
+    private final UserMapper userMapper;
+    //TODO createUser deleteUser findById findByUsername listallusers
+    // apply logs in all methods
+    // apply transactional in all methods
+
+    @Transactional
+    public UserResponse createUser(UserRequest userRequest){
+        User user = userMapper.toEntity(userRequest);
+        User savedUser = userDAO.save(user);
+        log.info("User created with the id: {}", savedUser.getId());
+        return userMapper.toResponse(savedUser);
+    }
 
 }
