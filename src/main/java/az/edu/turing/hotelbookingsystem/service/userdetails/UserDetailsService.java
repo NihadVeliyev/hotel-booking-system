06f0946c2245a807827766e45599c1d7e7bec41c
NihadVeliyev <nihadv795@gmail.com>
2025-08-29 23:50:54 +0400

    add UserDetailsService interface for user detail retrieval

---
+++ src/main/java/az/edu/turing/hotelbookingsystem/service/userdetails/UserDetailsService.java
@@ -0,0 +1,8 @@
+package az.edu.turing.hotelbookingsystem.service.userdetails;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
