15df31dc270d94eebb156a8af624de0f97492684
NihadVeliyev <nihadv795@gmail.com>
2025-08-30 23:40:27 +0400

    implement UserDetailsServiceImpl for user detail retrieval

--- src/main/java/az/edu/turing/hotelbookingsystem/security/UserDetailsServiceImpl.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/security/UserDetailsServiceImpl.java
@@ -1,6 +1,16 @@
 package az.edu.turing.hotelbookingsystem.security;
+import az.edu.turing.hotelbookingsystem.dao.UserDAO;
+import az.edu.turing.hotelbookingsystem.service.userdetails.UserDetailsService;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import az.edu.turing.hotelbookingsystem.service.UserService;
+@RequiredArgsConstructor
+@Data
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserDAO userDAO;
 
-public class UserDetailsServiceImpl extends UserService {
 }
