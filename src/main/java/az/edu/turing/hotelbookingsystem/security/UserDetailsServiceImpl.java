e243f75507ad43d5d9db5500e8b53d0d04bfdf98
NihadVeliyev <nihadv795@gmail.com>
2025-08-31 23:49:09 +0400

    implement loadUserByUsername method in UserDetailsServiceImpl for user retrieval

--- src/main/java/az/edu/turing/hotelbookingsystem/security/UserDetailsServiceImpl.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/security/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package az.edu.turing.hotelbookingsystem.security;
 import az.edu.turing.hotelbookingsystem.dao.UserDAO;
+import az.edu.turing.hotelbookingsystem.entity.User;
 import az.edu.turing.hotelbookingsystem.service.userdetails.UserDetailsService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -13,4 +14,17 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserDAO userDAO;
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user=userDAO.findUserByUsername(username);
+        if(user==null){
+            throw new UsernameNotFoundException("User not found with the username: "+username);
+        }
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+
+    }
 }
