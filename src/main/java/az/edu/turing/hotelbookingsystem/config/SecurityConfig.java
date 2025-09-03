9285ed2c1efb6c5831ab6d8e913d7e0032154240
NihadVeliyev <nihadv795@gmail.com>
2025-09-03 21:29:02 +0400

    add SecurityFilterChain bean to configure HTTP security

--- src/main/java/az/edu/turing/hotelbookingsystem/config/SecurityConfig.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package az.edu.turing.hotelbookingsystem.config;
 
 import az.edu.turing.hotelbookingsystem.security.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -12,5 +16,15 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig {
     private final UserDetailsServiceImpl userDetailsService;
     private final PasswordEncoder passwordEncoder;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .authorizeHttpRequests(auth->auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+
+    }
 
 }
