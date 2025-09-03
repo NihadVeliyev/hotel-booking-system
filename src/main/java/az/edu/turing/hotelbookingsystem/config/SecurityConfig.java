2bcb5db259be516a5062bef38598ff2cd390a5bd
NihadVeliyev <nihadv795@gmail.com>
2025-09-03 21:15:31 +0400

    add UserDetailsServiceImpl and PasswordEncoder to SecurityConfig

--- src/main/java/az/edu/turing/hotelbookingsystem/config/SecurityConfig.java
+++ src/main/java/az/edu/turing/hotelbookingsystem/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package az.edu.turing.hotelbookingsystem.config;
 
+import az.edu.turing.hotelbookingsystem.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
 }
